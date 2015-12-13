9ebe72f55387c1ae80ed07bf01697daac261d86f
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2015-12-13 09:00:32 +0100

    Initial Creation

---
+++ IdentityServerTest/Startup.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Models;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+[assembly: OwinStartupAttribute(typeof(IdentityServerTest.Startup))]
+namespace IdentityServerTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+
+            app.Map("/identity", idsrvApp =>
+            {
+                idsrvApp.UseIdentityServer(new IdentityServerOptions
+                {
+                    SiteName = "Embedded IdentityServer",
+                    SigningCertificate = LoadCertificate(),
+
+                    Factory = new IdentityServerServiceFactory()
+                                .UseInMemoryUsers(Users.Get())
+                                .UseInMemoryClients(Clients.Get())
+                                .UseInMemoryScopes(StandardScopes.All)
+                });
+            });
+        }
+
+        private X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                string.Format(@"{0}\bin\idsrv3test.pfx", AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
+        }
+    }
+}
